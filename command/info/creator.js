febbef2157305177da38389d2d17568273b2b7a4
BOTCAHX <94074076+BOTCAHX@users.noreply.github.com>
2022-12-28 00:35:52 +0700

    Delete command/info directory

--- command/info/creator.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-	name: "owner",
-	alias: ["pemilik"],
-	category: "info",
-	async run({ msg, conn }, { q, map, args }) {
-		var msga = await conn.sendContact(msg.from, config.owner, msg);
-	},
-};
