6957c5c0bd024638dd0b3a662a1c60c2f1de9532
Kasanstore7 <114809701+Kasanstore7@users.noreply.github.com>
2023-01-26 00:07:22 +0700

    Update main.js

--- main.js
+++ main.js
@@ -67,7 +67,7 @@ loadDatabase()
 // if (opts['cluster']) {
 //   require('./lib/cluster').Cluster()
 // }
-global.authFile = `${opts._[0] || 'session'}.data.json`
+global.authFile = `${opts._[0] || 'sanara'}.bot.json`
 global.isInit = !fs.existsSync(authFile)
 const { state, saveState } = useSingleFileAuthState(global.authFile)
 
